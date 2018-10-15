64532a6a571cb232a0c695d87dbdab2d2b4e56ad
abhi <abhi1996ok@gmail.com>
2018-10-16 03:16:53 +0530

    new java script file is added to the clone

---
+++ myscript.js
@@ -0,0 +1,19 @@
+function tableshow()
+{
+  var =x;
+   x =document.getElementById('dropTable');
+    if (x.style.display('none')) {
+        X.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+ var x1=document.getElementById('more');
+ x1.addEventListener('onclick',tableshow);
+ function nothing()
+  {
+	  alert("the script is applied on to the page !");
+  }
+   
+    window.onload=nothing;
