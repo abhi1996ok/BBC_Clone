3db7b8df533d8a203de9e44776dad155d7656fb0
abhi <abhi1996ok@gmail.com>
2018-10-16 04:12:34 +0530

    some new changes are made to the clone!

--- myscript.js
+++ myscript.js
@@ -1,19 +1,19 @@
-function tableshow()
-{
-  var =x;
-   x =document.getElementById('dropTable');
-    if (x.style.display('none')) {
-        X.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-
- var x1=document.getElementById('more');
- x1.addEventListener('onclick',tableshow);
+ function main(){
+	 var c =document.getElementById('dropTable');
+	   
+	  if(c.style.display === "none")
+	  {
+		  c.style.display="block";
+	  }
+	  else{
+		  c.style.display="none";
+	  }
+ }
  function nothing()
   {
-	  alert("the script is applied on to the page !");
+	  var x=document.getElementById('drop');
+	  alert("Welcome to the BBC news!")
+	  x.onclick=main;
   }
-   
-    window.onload=nothing;
+    
+	 window.addEventListener("load",nothing,false);
